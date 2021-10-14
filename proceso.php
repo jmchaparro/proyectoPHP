d2434a6801d333b09da21a91e792c3ca2352154c
DeLaHoz73 <90298773+DeLaHoz73@users.noreply.github.com>
2021-10-13 22:07:10 -0500

    Update proceso.php
    Cambio linea de conexión y eliminación de información de guía en linea 10

--- proceso.php
+++ proceso.php
@@ -4,8 +4,8 @@ $nombre=$_POST['nombre'];
 $telefono=$_POST['telefono'];
 $correo=$_POST['correo'];
 $tabla=$_POST['consultoria'];
-$conexion=mysqli_connect('localhost','root','agendador.5','asesorias');
+include 'conecta.php';
 $sql="UPDATE $tabla SET nombre = '$nombre', telefono = '$telefono', email = '$correo', estado = 1 WHERE id = $idcita";
 $consulta=mysqli_query($conexion,$sql);
-echo $idcita.$tabla."  Cita agendada correctamente, recibirá instrucciones en su correo"; 
+echo "  Cita agendada correctamente, recibirá instrucciones en su correo"; 
 ?>
